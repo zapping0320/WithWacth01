85b39b89ef47888099bc6b4dc797f1d358894612
John DongHyun Kim <zapping0320@gmail.com>
2020-11-14 19:52:27 +0900

    watch target 추가
    - watchkit
    - storyboard

---
+++ WithWacthApp01 Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  WithWacthApp01 Extension
+//
+//  Created by DONGHYUN KIM on 2020/11/14.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ WithWacthApp01 Extension/NotificationController.swift
@@ -0,0 +1,34 @@
+//
+//  NotificationController.swift
+//  WithWacthApp01 Extension
+//
+//  Created by DONGHYUN KIM on 2020/11/14.
+//
+
+import WatchKit
+import Foundation
+import UserNotifications
+
+class NotificationController: WKUserNotificationInterfaceController {
+
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
